bf787021f09101ad286705eeb5f270dec4244b7f
Ash031 <a77284@alunos.uminho.pt>
2019-08-17 21:22:26 +0100

    Fixed Shop

--- scripts/Shop.js
+++ scripts/Shop.js
@@ -1,17 +1,17 @@
 function printShop(){
     var string = "<h1 style='text-align:center'>Welcome to The Shop</h1><hr/>";
     string += "<table class='w3-table-all'>";
-    string += "<tr><td><p>+1 Attack</p><button onclick='buy('1Attack')'>20 Moneys</button></td><td>"
-    string += "<p>+1 HP</p><button onclick='buy('1HP')'>10 Moneys</button></td><td>"
-    string += "<p>+1 Defense</p><button onclick='buy('1Defense')'>30 Moneys</button></td></tr>"
-    string += "<tr><td><p>+10 Attack</p><button onclick='buy('10Attack')'>200 Moneys</button></td><td>"
-    string += "<p>+10 HP</p><button onclick='buy('10HP')'>100 Moneys</button></td><td>"
-    string += "<p>+10 Defense</p><button onclick='buy('10Defense')'>300 Moneys</button></td></tr>"
+    string += "<tr><td><p>+1 Attack</p><button onclick=\"buy('1Attack')\">20 Moneys</button></td><td>"
+    string += "<p>+1 HP</p><button onclick=\"buy('1HP')\">10 Moneys</button></td><td>"
+    string += "<p>+1 Defense</p><button onclick=\"buy('1Defense')\">30 Moneys</button></td></tr>"
+    string += "<tr><td><p>+10 Attack</p><button onclick=\"buy('10Attack')\">200 Moneys</button></td><td>"
+    string += "<p>+10 HP</p><button onclick=\"buy('10HP')\">100 Moneys</button></td><td>"
+    string += "<p>+10 Defense</p><button onclick=\"buy('10Defense')\">300 Moneys</button></td></tr>"
     string += "<tr><td><p>+"+printNumber(Math.floor(player.money/20))+" Attack</p><button onclick=\"buyMax('Attack',"+Math.floor(player.money/20)+")\">"+printNumber(player.money-player.money%20)+" Moneys</button></td><td>"
     string += "<p>+"+printNumber(Math.floor(player.money/10))+" HP</p><button onclick=\"buyMax('HP',"+Math.floor(player.money/10)+")\">"+printNumber(player.money-player.money%10)+" Moneys</button></td><td>"
     string += "<p>+"+printNumber(Math.floor(player.money/30))+" Defense</p><button onclick=\"buyMax('Defense',"+Math.floor(player.money/30)+")\">"+printNumber(player.money-player.money%30)+" Moneys</button></td></tr>"
-    string += "<tr><td><p>+1 Clone</p><button onclick='buy('1Clone')'>150 Moneys</button></td>"
-    string += "<td><p>+10 Clone</p><button onclick='buy('10Clone')'>1500 Moneys</button></td>"
+    string += "<tr><td><p>+1 Clone</p><button onclick=\"buy('1Clone')\">150 Moneys</button></td>"
+    string += "<td><p>+10 Clone</p><button onclick=\"buy('10Clone')\">1500 Moneys</button></td>"
     string += "<td><p>+"+printNumber(Math.floor(player.money/150))+" Clone</p><button onclick=\"buyMax('Clone',"+Math.floor(player.money/150)+")\">"+printNumber(player.money-player.money%150)+" Moneys</button></td>"
     string += "</tr></table>";
     document.getElementById('Screen').innerHTML=string;
